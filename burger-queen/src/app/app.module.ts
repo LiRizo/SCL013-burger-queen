2478ea7c1f4d626f65ab6bcfbcc56a0f054e5a0a
yarimar-labo <ydiazm1990@gmail.com>
2020-07-03 13:05:26 -0400

    Se crearon rutas de las vistas

--- burger-queen/src/app/app.module.ts
+++ burger-queen/src/app/app.module.ts
@@ -6,13 +6,43 @@ import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { environment } from '../environments/environment';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { IngresoComponent } from './components/ingreso/ingreso.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { CocinaComponent } from './components/cocina/cocina.component';
-import { OrdenesListasComponent } from './components/ordenes-listas/ordenes-listas.component';
+
+import { RouterModule, Routes} from '@angular/router';
+
+const rutas: Routes = [
+
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+
+  {
+    path: 'ingreso',
+    component: IngresoComponent
+  },
+
+  {
+    path: 'menu',
+    component: MenuComponent
+  },
+
+  {
+    path: 'cocina',
+    component: CocinaComponent
+  },
+
+]
 
 @NgModule({
   declarations: [
@@ -21,14 +51,13 @@ import { OrdenesListasComponent } from './components/ordenes-listas/ordenes-list
     IngresoComponent,
     MenuComponent,
     CocinaComponent,
-    OrdenesListasComponent,
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFirestoreModule,
-    AppRoutingModule
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]

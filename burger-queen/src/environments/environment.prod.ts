be0983675d64519ed50bce77e7711ca5783095fe
Your Name <you@example.com>
2020-07-19 17:58:14 -0400

    deploy

--- burger-queen/src/environments/environment.prod.ts
+++ burger-queen/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDOXNM4GEYzygiW7IGWSclYUByfSWXjSOo",
+    authDomain: "moongold-lab.firebaseapp.com",
+    databaseURL: "https://moongold-lab.firebaseio.com",
+    projectId: "moongold-lab",
+    storageBucket: "moongold-lab.appspot.com",
+    messagingSenderId: "105589804895",
+    appId: "1:105589804895:web:68b155f924e1e1222970ea",
+    measurementId: "G-EG8QN1YZ5Q"
+  },
+
 };
